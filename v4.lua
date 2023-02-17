0df4ea21346b9853715a6ada32711913326978fe
lilmond <54730301+lilmond@users.noreply.github.com>
2023-02-17 12:07:39 +0800

    Create v4.lua

---
+++ v4.lua
@@ -0,0 +1,143 @@
+--- Configs
+local flybutton = "e"
+local flyspeed = 100
+local controls = {
+	front = "w",
+	back = "s",
+	right = "d",
+	left = "a",
+	up = "space",
+	down = "leftcontrol",
+	add_speed = "rightbracket",
+	sub_speed = "leftbracket",
+	reset_speed = "minus"
+}
+local default_flyspeed = flyspeed
+-- Configs
+
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+local runservice = game:GetService("RunService")
+local uis = game:GetService("UserInputService")
+
+local flycontrol = {F = 0, R = 0, B = 0, L = 0, U = 0, D = 0}
+local flying = false
+
+local function fly()
+	local character = player.Character
+	if not character then return end
+	local hrp = character:FindFirstChild("HumanoidRootPart")
+	if not hrp then return end
+	local humanoid = character:FindFirstChildWhichIsA("Humanoid")
+	if not humanoid then return end
+
+	flying = true
+
+	local bv = Instance.new("BodyVelocity")
+	local bg = Instance.new("BodyGyro")
+	bv.MaxForce = Vector3.new(9e4, 9e4, 9e4)
+	bg.CFrame = hrp.CFrame
+	bg.MaxTorque = Vector3.new(9e4, 9e4, 9e4)
+	bg.P = 9e4
+	bv.Parent = hrp
+	bg.Parent = hrp
+
+	for i, child in pairs(character:GetDescendants()) do
+		if child:IsA("BasePart") then
+			coroutine.wrap(function()
+				local con = nil
+				con = runservice.Stepped:Connect(function()
+					if not flying then
+						con:Disconnect()
+						child.CanCollide = true
+					end
+					child.CanCollide = false
+				end)
+			end)()
+		end
+	end
+
+	local con = nil
+	con = runservice.Stepped:Connect(function()
+		if not flying then
+			con:Disconnect()
+			bv:Destroy()
+			bg:Destroy()
+		end
+
+		humanoid.PlatformStand = true
+		bv.Velocity = (workspace.Camera.CoordinateFrame.LookVector * ((flycontrol.F - flycontrol.B) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.RightVector * ((flycontrol.R - flycontrol.L) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.UpVector * ((flycontrol.U - flycontrol.D) * flyspeed))
+		bg.CFrame = workspace.Camera.CoordinateFrame
+	end)
+
+	repeat wait() until not flying
+
+	while humanoid.PlatformStand == true do
+		humanoid.PlatformStand = false
+		task.wait()
+	end
+end
+
+uis.InputBegan:Connect(function(key, paused)
+	if paused then return end
+	
+	key = key.KeyCode.Name:lower()
+
+	if key == flybutton then
+		if flying then
+			flying = false
+		else
+			fly()
+		end
+	elseif key == controls.front then
+		flycontrol.F = 1
+	elseif key == controls.back then
+		flycontrol.B = 1
+	elseif key == controls.right then
+		flycontrol.R = 1
+	elseif key == controls.left then
+		flycontrol.L = 1
+	elseif key == controls.up then
+		flycontrol.U = 1
+	elseif key == controls.down then
+		flycontrol.D = 1
+	elseif key == controls.add_speed then
+		if flyspeed == 1 then
+			flyspeed = 25
+		else
+			flyspeed += 25
+		end
+	elseif key == controls.sub_speed then
+		if (flyspeed - 25) < 1 then
+			flyspeed = 1
+		else	
+			flyspeed -= 25
+		end
+	elseif key == controls.reset_speed then
+		flyspeed = default_flyspeed
+	end
+end)
+
+uis.InputEnded:Connect(function(key, paused)
+	if paused then return end
+	
+	key = key.KeyCode.Name:lower()
+	
+	if key == controls.front then
+		flycontrol.F = 0
+	elseif key == controls.back then
+		flycontrol.B = 0
+	elseif key == controls.right then
+		flycontrol.R = 0
+	elseif key == controls.left then
+		flycontrol.L = 0
+	elseif key == controls.up then
+		flycontrol.U = 0
+	elseif key == controls.down then
+		flycontrol.D = 0
+	end
+end)
+
+player.CharacterAdded:Connect(function()
+	flying = false
+end)
