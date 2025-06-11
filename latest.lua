eb1b6d8295aef6582e8928ab0cde2a74d235c733
₱ØⱠɎ₥ØⱤ₱Ⱨł₵ <54730301+lilmond@users.noreply.github.com>
2025-06-11 18:48:34 +0000

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -1,16 +1,17 @@
--- VERSION: 4.2.2.6
+-- VERSION: 4.2.2.7
 -- More versions: https://github.com/lilmond/roblox_fly_script
 
 --- Configs
-local flybutton = "t"
-local flyparent = "HumanoidRootPart"
-local flyspeed = 100
+getgenv().fly_force_stop = false
+getgenv().flybutton = "t"
+getgenv().flyparent = "HumanoidRootPart"
+getgenv().flyspeed = 100
 
-local invisible_subkey = ""
-local invisiblebutton = "y"
-local invisible_max_distance = 9e10
+getgenv().invisible_subkey = ""
+getgenv().invisiblebutton = "y"
+getgenv().invisible_max_distance = 9e10
 
-local controls = {
+getgenv().controls = {
 	front = "w",
 	back = "s",
 	right = "d",
@@ -21,7 +22,7 @@ local controls = {
 	sub_speed = "leftbracket",
 	reset_speed = "minus"
 }
-local default_flyspeed = flyspeed
+getgenv().default_flyspeed = getgenv().flyspeed
 -- Configs
 
 local player = game:GetService("Players").LocalPlayer
@@ -37,7 +38,7 @@ local invisible_enabled = false
 local function fly()
 	local character = player.Character
 	if not character then return end
-	local hrp = character:FindFirstChild(flyparent)
+	local hrp = character:FindFirstChild(getgenv().flyparent)
 	if not hrp then return end
 	local humanoid = character:FindFirstChildWhichIsA("Humanoid")
 	if not humanoid then return end
@@ -79,7 +80,7 @@ local function fly()
 		end
 		
 		humanoid.PlatformStand = true
-		bv.Velocity = (workspace.Camera.CoordinateFrame.LookVector * ((flycontrol.F - flycontrol.B) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.RightVector * ((flycontrol.R - flycontrol.L) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.UpVector * ((flycontrol.U - flycontrol.D) * flyspeed))
+		bv.Velocity = (workspace.Camera.CoordinateFrame.LookVector * ((flycontrol.F - flycontrol.B) * getgenv().flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.RightVector * ((flycontrol.R - flycontrol.L) * getgenv().flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.UpVector * ((flycontrol.U - flycontrol.D) * getgenv().flyspeed))
 		bg.CFrame = workspace.Camera.CoordinateFrame
 	end)
 
@@ -149,146 +150,175 @@ local function invisible()
 			con:Disconnect()
 			return
 		end
-		hrp.CFrame = CFrame.new(random:NextNumber(-invisible_max_distance, invisible_max_distance), random:NextNumber(0, invisible_max_distance), random:NextNumber(-invisible_max_distance, invisible_max_distance))
+		hrp.CFrame = CFrame.new(random:NextNumber(-getgenv().invisible_max_distance, getgenv().invisible_max_distance), random:NextNumber(0, getgenv().invisible_max_distance), random:NextNumber(-getgenv().invisible_max_distance, getgenv().invisible_max_distance))
 		hrp.AssemblyLinearVelocity = Vector3.new(0,0,0)
-		bv.Velocity = (workspace.Camera.CoordinateFrame.LookVector * ((flycontrol.F - flycontrol.B) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.RightVector * ((flycontrol.R - flycontrol.L) * flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.UpVector * ((flycontrol.U - flycontrol.D) * flyspeed))
+		bv.Velocity = (workspace.Camera.CoordinateFrame.LookVector * ((flycontrol.F - flycontrol.B) * getgenv().flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.RightVector * ((flycontrol.R - flycontrol.L) * getgenv().flyspeed)) + (workspace.CurrentCamera.CoordinateFrame.UpVector * ((flycontrol.U - flycontrol.D) * getgenv().flyspeed))
 		bg.CFrame = workspace.Camera.CoordinateFrame
 	end)
 end
 
-uis.InputBegan:Connect(function(keyinput, paused)
-	if paused then return end
+local function main()
+	if getgenv().FLY_SCRIPT_RUNNING then return end
+	getgenv().FLY_SCRIPT_RUNNING = true
 
-	local key = keyinput.KeyCode.Name:lower()
+	getgenv().KEY_LISTENER1 = uis.InputBegan:Connect(function(keyinput, paused)
+		if paused then return end
 
-	if key == flybutton then
-		if flying then
-			flying = false
-		else
-			fly()
-		end
-	elseif key == controls.front then
-		flycontrol.F = 1
-	elseif key == controls.back then
-		flycontrol.B = 1
-	elseif key == controls.right then
-		flycontrol.R = 1
-	elseif key == controls.left then
-		flycontrol.L = 1
-	elseif key == controls.up then
-		flycontrol.U = 1
-	elseif key == controls.down then
-		flycontrol.D = 1
-	elseif key == controls.add_speed then
-		if flyspeed == 1 then
-			flyspeed = 25
-		else
-			flyspeed += 25
-		end
-		textgui.TextTransparency = 0
-		textgui.Text = flyspeed
-		wait(0.1)
-		while uis:IsKeyDown(keyinput.KeyCode) do
-			flyspeed += 25
-			textgui.Text = flyspeed
-			wait(0.05)
-		end
-		textgui.TextTransparency = 1
-	elseif key == controls.sub_speed then
-		if (flyspeed - 25) < 1 then
-			flyspeed = 1
-		else	
-			flyspeed -= 25
-		end
-		textgui.TextTransparency = 0
-		textgui.Text = flyspeed
-		wait(0.1)
-		while uis:IsKeyDown(keyinput.KeyCode) do
-			if (flyspeed - 25) < 1 then
-				flyspeed = 1
+		local key = keyinput.KeyCode.Name:lower()
+
+		if key == getgenv().flybutton then
+			if flying then
+				flying = false
+			else
+				fly()
+			end
+		elseif key == getgenv().controls.front then
+			flycontrol.F = 1
+		elseif key == getgenv().controls.back then
+			flycontrol.B = 1
+		elseif key == getgenv().controls.right then
+			flycontrol.R = 1
+		elseif key == getgenv().controls.left then
+			flycontrol.L = 1
+		elseif key == getgenv().controls.up then
+			flycontrol.U = 1
+		elseif key == getgenv().controls.down then
+			flycontrol.D = 1
+		elseif key == getgenv().controls.add_speed then
+			if getgenv().flyspeed == 1 then
+				getgenv().flyspeed = 25
+			else
+				getgenv().flyspeed += 25
+			end
+			textgui.TextTransparency = 0
+			textgui.Text = getgenv().flyspeed
+			wait(0.1)
+			while uis:IsKeyDown(keyinput.KeyCode) do
+				getgenv().flyspeed += 25
+				textgui.Text = getgenv().flyspeed
+				wait(0.05)
+			end
+			textgui.TextTransparency = 1
+		elseif key == getgenv().controls.sub_speed then
+			if (getgenv().flyspeed - 25) < 1 then
+				getgenv().flyspeed = 1
 			else	
-				flyspeed -= 25
+				getgenv().flyspeed -= 25
 			end
-			textgui.Text = flyspeed
-			wait(0.05)
-		end
-		textgui.TextTransparency = 1
-	elseif key == controls.reset_speed then
-		textgui.TextTransparency = 0
-		flyspeed = default_flyspeed
-		textgui.Text = flyspeed
-		wait(0.1)
-		textgui.TextTransparency = 1
-	elseif key == invisiblebutton then
-		local subkey_held = false
-		
-		if (invisible_subkey ~= "") then
-			for i, kc in pairs(Enum.KeyCode:GetEnumItems()) do
-				local kcs = kc.Name:lower()
-				if (kcs == invisible_subkey) then
-					if not uis:IsKeyDown(kc) then return end
-					subkey_held = true
+			textgui.TextTransparency = 0
+			textgui.Text = getgenv().flyspeed
+			wait(0.1)
+			while uis:IsKeyDown(keyinput.KeyCode) do
+				if (getgenv().flyspeed - 25) < 1 then
+					getgenv().flyspeed = 1
+				else	
+					getgenv().flyspeed -= 25
 				end
+				textgui.Text = getgenv().flyspeed
+				wait(0.05)
 			end
-		else
-			subkey_held = true
+			textgui.TextTransparency = 1
+		elseif key == getgenv().controls.reset_speed then
+			textgui.TextTransparency = 0
+			getgenv().flyspeed = getgenv().default_flyspeed
+			textgui.Text = getgenv().flyspeed
+			wait(0.1)
+			textgui.TextTransparency = 1
+		elseif key == getgenv().invisiblebutton then
+			local subkey_held = false
+			
+			if (getgenv().invisible_subkey ~= "") then
+				for i, kc in pairs(Enum.KeyCode:GetEnumItems()) do
+					local kcs = kc.Name:lower()
+					if (kcs == getgenv().invisible_subkey) then
+						if not uis:IsKeyDown(kc) then return end
+						subkey_held = true
+					end
+				end
+			else
+				subkey_held = true
+			end
+			
+			if not subkey_held then return end
+			
+			if invisible_enabled then
+				invisible_enabled = false
+			else
+				invisible()
+			end
 		end
-		
-		if not subkey_held then return end
-		
-		if invisible_enabled then
-			invisible_enabled = false
-		else
-			invisible()
+	end)
+
+	getgenv().KEY_LISTENER2 = uis.InputEnded:Connect(function(key, paused)
+		if paused then return end
+
+		key = key.KeyCode.Name:lower()
+
+		if key == getgenv().controls.front then
+			flycontrol.F = 0
+		elseif key == getgenv().controls.back then
+			flycontrol.B = 0
+		elseif key == getgenv().controls.right then
+			flycontrol.R = 0
+		elseif key == getgenv().controls.left then
+			flycontrol.L = 0
+		elseif key == getgenv().controls.up then
+			flycontrol.U = 0
+		elseif key == getgenv().controls.down then
+			flycontrol.D = 0
 		end
+	end)
+
+	local function load_textgui()
+		local gui = player.PlayerGui:FindFirstChild("FlyGUI")
+		if gui then
+			gui:Destroy()
+		end
+
+		local gui = Instance.new("ScreenGui")
+		local text = Instance.new("TextLabel")
+
+		gui.Name = "FlyGUI"
+		gui.ResetOnSpawn = false
+		gui.IgnoreGuiInset = true
+
+		text.Size = UDim2.new(1, 0, 0, 25)
+		text.Position = UDim2.fromScale(0, 0.5)
+		text.BackgroundTransparency = 1
+		text.TextSize = 25
+		text.Text = getgenv().flyspeed
+		text.TextColor = BrickColor.new("White")
+		text.Font = Enum.Font.SciFi
+		text.TextTransparency = 1
+
+		gui.Parent = player.PlayerGui
+		text.Parent = gui
+
+		return text
 	end
-end)
 
-uis.InputEnded:Connect(function(key, paused)
-	if paused then return end
+	getgenv().CHARACTER_ADDED_CON = player.CharacterAdded:Connect(function()
+		flying = false
+		invisible_enabled = false
+	end)
 
-	key = key.KeyCode.Name:lower()
-
-	if key == controls.front then
-		flycontrol.F = 0
-	elseif key == controls.back then
-		flycontrol.B = 0
-	elseif key == controls.right then
-		flycontrol.R = 0
-	elseif key == controls.left then
-		flycontrol.L = 0
-	elseif key == controls.up then
-		flycontrol.U = 0
-	elseif key == controls.down then
-		flycontrol.D = 0
-	end
-end)
-
-local function load_textgui()
-	local gui = Instance.new("ScreenGui")
-	local text = Instance.new("TextLabel")
-
-	gui.ResetOnSpawn = false
-	gui.IgnoreGuiInset = true
-
-	text.Size = UDim2.new(1, 0, 0, 25)
-	text.Position = UDim2.fromScale(0, 0.5)
-	text.BackgroundTransparency = 1
-	text.TextSize = 25
-	text.Text = flyspeed
-	text.TextColor = BrickColor.new("White")
-	text.Font = Enum.Font.SciFi
-	text.TextTransparency = 1
-
-	gui.Parent = game:GetService("Players").LocalPlayer.PlayerGui
-	text.Parent = gui
-
-	return text
+	textgui = load_textgui()
 end
 
-player.CharacterAdded:Connect(function()
-	flying = false
-	invisible_enabled = false
-end)
+if (not getgenv().fly_force_stop) then
+	main()
+else
+	if getgenv().KEY_LISTENER1 then
+		getgenv().KEY_LISTENER1:Disconnect()
+	end
 
-textgui = load_textgui()
+	if getgenv().KEY_LISTENER2 then
+		getgenv().KEY_LISTENER2:Disconnect()
+	end
+
+	if getgenv().CHARACTER_ADDED_CON then
+		getgenv().CHARACTER_ADDED_CON:Disconnect()
+	end
+
+	getgenv().FLY_SCRIPT_RUNNING = false
+end
